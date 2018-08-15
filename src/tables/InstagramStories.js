05f050c7ab4ff7a57efe0ea2439c3a217e71d7ec
Peter Heesterman <heesterman.peter@gmail.com>
2018-08-15 12:42:15 +1200

    Add instagramStories table

---
+++ src/tables/InstagramStories.js
@@ -0,0 +1,21 @@
+// @flow
+
+import faker from 'faker'
+
+const table = () => ({
+  key: 'instagramStory',
+  tableName: 'InstagramStories',
+  foreignKeys: {
+    instagramAccountId: ['InstagramAccounts', 'id'],
+    contentId: ['Content', 'id'],
+  },
+  columns: {
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.past(),
+    name: `${faker.name.findName()}'s snap story`,
+    storyDate: faker.date.past(),
+    archived: faker.random.boolean()
+  }
+})
+
+export default table
