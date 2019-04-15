e60e64d173cbc4dddacc11a83914a88b57d7be1b
George Czabania <george@mish.guru>
2019-04-16 00:37:55 +1200

    Update dependencies

--- testHelpers/afterAll.js
+++ testHelpers/afterAll.js
@@ -1,4 +1,4 @@
-const db = require('@mishguru/data')
+const { default: db } = require('@mishguru/data')
 const { teardownTestDatabase, quit } = require('@mishguru/test-helpers')
 
 teardownTestDatabase(db).then(quit)
--- testHelpers/beforeAll.js
+++ testHelpers/beforeAll.js
@@ -1,4 +1,4 @@
-const db = require('@mishguru/data')
+const { default: db } = require('@mishguru/data')
 const { setupTestDatabase, quit } = require('@mishguru/test-helpers')
 
 setupTestDatabase(db).then(quit)
